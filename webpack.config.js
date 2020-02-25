18afcdd5115d52da4e4894b721b9bf89299524f0
Reynaldo Layme <layme.reynaldo@icloud.com>
2020-02-24 21:09:51 -0300

    fix(setup-configuration): fixed of webpack bundle mode development
    Corrected the call of the file generated by webpack, corrected the output and created the index html
    file to call this bundle.

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: path.resolve(__dirname, 'src', 'index.js'),
-  outout: {
+  output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js'
   },
